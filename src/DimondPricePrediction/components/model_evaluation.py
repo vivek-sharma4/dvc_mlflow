41e94dcbc14a257b085e52ce7be498d05c6bf72d
Vivek Sharma <viveksharma432002@gmail.com>
2024-03-08 22:40:14 +0530

    add file

--- src/DimondPricePrediction/components/model_evaluation.py
+++ src/DimondPricePrediction/components/model_evaluation.py
@@ -24,11 +24,7 @@ class ModelEvaluation:
 
     def initiate_model_evaluation(self,train_array,test_array):
         try:
-            x_train,y_train,x_test,y_test=(
-                test_array[:,:-1], 
-                test_array[:,-1],
-                test_array[:,:-1],
-                test_array[:,-1])
+            x_test,y_test=(test_array[:,:-1],test_array[:,-1])
 
             model_path=os.path.join("artifacts","model.pkl")
             model=load_object(model_path)
